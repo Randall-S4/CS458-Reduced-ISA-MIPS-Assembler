ec7a6226e9a6803e2e5b8cd3719a20343f607fdd
Randall Sanders <randallds2@gmail.com>
2024-09-25 14:43:32 -0500

    initial hashmap logic

--- Mips_Assembler.java
+++ Mips_Assembler.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+
 public class Mips_Assembler {
+    private static HashMap<String, Integer> opcodeMap = new HashMap<>();
+    private static HashMap<String, Integer> functMap = new HashMap<>();
+
+    static {
+        initializeMaps();
+    }
+
+    private static void initializeMaps() {
+        opcodeMap.put("add", 0);
+        functMap.put("add", 32);
+        opcodeMap.put("addiu", 9);
+    }
 }
